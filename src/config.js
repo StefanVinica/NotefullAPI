e19acdadf8b0d1335fbaf2699e5d99f6200496a5
StefanVinica <stefanikolovski996@gmail.com>
2020-11-04 18:14:30 -0800

    production

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 module.exports = {
-    PORT: process.env.PORT,
-    NODE_ENV: process.env.NODE_ENV,
-    API_TOKEN: process.env.API_TOKEN,
+    PORT: process.env.PORT || 8000,
+    NODE_ENV: process.env.NODE_ENV || 'development',
+    API_TOKEN: process.env.API_TOKEN || 'secret',
     DB_URL: process.env.DB_URL,
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://stefan@localhost/Notefull',
   }
